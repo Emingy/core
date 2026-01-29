048b3e70cc395fbbd9ab2c00d994754d75f5461a
Александр Потапов <shuraken.yeah@gmail.com>
2026-01-29 11:26:38 +0300

    chore(Icon): export type icon added

--- src/ui/Icon/src/index.tsx
+++ src/ui/Icon/src/index.tsx
@@ -4,7 +4,7 @@ import { createElement } from 'react';
 import styles from './index.module.scss';
 
 import { EColor, ESize } from './constants';
-import type { TProps } from './types.ts';
+import type { TProps, TSvgComponent } from './types.ts';
 
 const BLOCK_NAME = 'Icon';
 const cn = cls.bind(styles);
@@ -17,14 +17,10 @@ export const Icon = ({
     ...restProps
 }: TProps) => {
     return createElement(icon, {
-        className: cn(
-            BLOCK_NAME,
-            `${BLOCK_NAME}__${size}`,
-            `${BLOCK_NAME}__${color}`,
-            className,
-        ),
+        className: cn(BLOCK_NAME, `${BLOCK_NAME}__${size}`, `${BLOCK_NAME}__${color}`, className),
         ...restProps,
     });
 };
 
 export type TIconProps = TProps;
+export type TIcon = TSvgComponent;
